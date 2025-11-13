26bbeb63f37cf88fda485f9ad9576a5318b4eb7e
abraaosantosdeveloper <abraaofilipi12@gmail.com>
2025-11-13 13:34:42 -0300

    All functions tested

--- index.js
+++ index.js
@@ -1,10 +1,66 @@
 /**** Escreva o código abaixo 👇******/
 
+function somar(a, b){
+  let result = a + b;
+  return result
+};
+
+function multiplicar(a, b){
+  let result = a * b;
+  return result
+};
+
+function subtrair(a, b){
+  let result = a - b;
+  return result
+};
+
+function dividir(a, b){
+  let result = a / b;
+  if(b === 0){
+    throw "Erro: divisão por zero.";
+  }
+  else{
+    return result
+  }
+  
+};
 
 
+function calcularMedia(nota1, nota2, nota3, nota4){
+  let numerador = (nota1 + nota2 + nota3 + nota4)
+  let result = dividir(numerador, 4);
+  return result
+};
 
+function elevarPotencia(a, b){
+  let result = Math.pow(a, b);
+  return result
+};
 
+function calcularAreaRetangulo(a, b){
+  let result = multiplicar(a, b);
+  return result
+};
 
+function verificarParOuImpar(numero){
+  if(numero%2 === 0){
+    return "par"
+  }
+  else{
+    return "ímpar"
+  }
+};
+
+function calcularRaizQuadrada(numero){
+  let result = Math.sqrt(numero);
+  return result
+};
+
+function calcularHipotenusa(cateto1, cateto2){
+  let result = Math.sqrt((Math.pow(cateto1, 2) + Math.pow(cateto2, 2)));
+  return result
+}
 
 
 /**** Escreva o código acima ☝️******/
